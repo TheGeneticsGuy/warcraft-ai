6863a540dc913fa3465c3e05a53fc71176731877
Aaron Topping <aaronjtopping@gmail.com>
2025-04-07 01:55:52 -0700

    Ready for deploy

--- src/js/blizzAPI.js
+++ src/js/blizzAPI.js
@@ -2,43 +2,43 @@
 import ENV from './env.js';
 
 async function getKeys() {
-    try {
-        return {
-            client_id: ENV.CLIENT_ID,
-            client_secret: ENV.CLIENT_SECRET
-        };
-    } catch (error) {
-        console.error('Error getting keys:', error);
-    }
+  try {
+    return {
+      client_id: ENV.CLIENT_ID,
+      client_secret: ENV.CLIENT_SECRET,
+    };
+  } catch (error) {
+    console.error('Error getting keys:', error);
+  }
 }
 
 async function getAccessToken() {
-    const keys = await getKeys();
-    console.log('Keys:', keys); // Debug: Check what client_id and client_secret are
-    if (keys) {
-        const { client_id, client_secret } = keys;
-        const url = 'https://us.battle.net/oauth/token';
-        const auth = btoa(`${client_id}:${client_secret}`);
-        console.log('Auth Header:', `Basic ${auth}`); // Debug: Check the encoded auth
-        try {
-            const tokenResponse = await fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Authorization': `Basic ${auth}`,
-                    'Content-Type': 'application/x-www-form-urlencoded'
-                },
-                body: 'grant_type=client_credentials'
-            });
-            if (tokenResponse.ok) {
-                const data = await tokenResponse.json();
-                return data.access_token;
-            } else {
-                throw new Error(await tokenResponse.text());
-            }
-        } catch (error) {
-            console.log(error);
-        }
+  const keys = await getKeys();
+  console.log('Keys:', keys); // Debug: Check what client_id and client_secret are
+  if (keys) {
+    const { client_id, client_secret } = keys;
+    const url = 'https://us.battle.net/oauth/token';
+    const auth = btoa(`${client_id}:${client_secret}`);
+    console.log('Auth Header:', `Basic ${auth}`); // Debug: Check the encoded auth
+    try {
+      const tokenResponse = await fetch(url, {
+        method: 'POST',
+        headers: {
+          Authorization: `Basic ${auth}`,
+          'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: 'grant_type=client_credentials',
+      });
+      if (tokenResponse.ok) {
+        const data = await tokenResponse.json();
+        return data.access_token;
+      } else {
+        throw new Error(await tokenResponse.text());
+      }
+    } catch (error) {
+      console.log(error);
     }
+  }
 }
 
 export { getAccessToken };

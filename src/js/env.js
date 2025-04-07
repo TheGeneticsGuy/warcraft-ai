17b85b3f29211549607b4edb5b18bc15d0dd51b6
Aaron Topping <aaronjtopping@gmail.com>
2025-04-07 00:51:50 -0700

    Build Ready for netlify deploy

--- src/js/env.js
+++ src/js/env.js
@@ -1,7 +1,7 @@
 console.log('VITE_CLIENT_ID:', import.meta.env.VITE_CLIENT_ID);
 console.log('VITE_CLIENT_SECRET:', import.meta.env.VITE_CLIENT_SECRET);
 const ENV = {
-    CLIENT_ID: import.meta.env.VITE_CLIENT_ID,
-    CLIENT_SECRET: import.meta.env.VITE_CLIENT_SECRET
+  CLIENT_ID: import.meta.env.VITE_CLIENT_ID,
+  CLIENT_SECRET: import.meta.env.VITE_CLIENT_SECRET,
 };
 export default ENV;
